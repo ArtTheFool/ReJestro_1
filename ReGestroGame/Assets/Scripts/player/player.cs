61483faeba330d39e38424e8159bece612a993ff
ArtTheFool <artemomelhenko248@gmail.com>
2026-01-16 20:23:45 +0200

    #9 Video
    Зробив меч, його анімацію та переробив спраут моба з слайма на скелета

--- ReGestroGame/Assets/Scripts/player/player.cs
+++ ReGestroGame/Assets/Scripts/player/player.cs
@@ -7,24 +7,34 @@ public class Player : MonoBehaviour {
     public static Player Instance { get; private set; }
 
    [SerializeField] private float movingSpeed = 10f;
+    Vector2 inputVector;
     private Rigidbody2D rb;
+
+    private float minMovingSpeed = 0.1f;
+    private bool isRunning = false;
+
     private void Awake() {
         Instance = this;
         rb = GetComponent<Rigidbody2D>();    
     }
+    private void Start() {
+        GameInput.Instance.OnPlayerAttack += Player_OnPlayerAttack;
+    }
 
-    private float minMovingSpeed = 0.1f;
-    private bool isRunning = false;
+    private void Player_OnPlayerAttack(object sender, System.EventArgs e) {
+     ActiveWeapon.Instance.GetActiveWepon().Attack();
+    }
+
+    private void Update() {
+      inputVector = GameInput.Instance.GetMovementVector();
+    }
+
     private void FixedUpdate() {
         HandleMovement();
     }
 
     private void HandleMovement() {
-        Vector2 inputVector = GameInput.Instance.GetMovementVector();
-        //inputVector = inputVector.normalized;
-        Debug.Log(inputVector);
         rb.MovePosition(rb.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
-
         if (Mathf.Abs(inputVector.x) > minMovingSpeed || Mathf.Abs(inputVector.y) > minMovingSpeed) {
             isRunning = true;
         }
