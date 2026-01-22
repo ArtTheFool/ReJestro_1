5b782b8ce48cb1199bcc135da57f7ac7ef825f15
borzyy221 <bogdanapodernya@gmail.com>
2026-01-22 17:32:37 +0200

    13

--- ReGestroGame/Assets/Scripts/Skeleton/EnemyEntity.cs
+++ ReGestroGame/Assets/Scripts/Skeleton/EnemyEntity.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PolygonCollider2D))]
+
 public class EnemyEntity : MonoBehaviour {
     [SerializeField] private int _maxHealth;
     private int _currentHealth;
+
+    private PolygonCollider2D _polygonCollider2D;
+
+    private void Awake() {
+        _polygonCollider2D = GetComponent<PolygonCollider2D>();
+    }
 
     private void Start() {
         _currentHealth = _maxHealth;
@@ -16,9 +24,18 @@ public class EnemyEntity : MonoBehaviour {
 
     }
 
-    public void DetectDeath() {
+    public void PolygonColliderTurnOff() {
+        _polygonCollider2D.enabled = false;
+    }
+
+    public void PolygonColliderTurnOn() {
+        _polygonCollider2D.enabled = true;
+    }
+
+    private void DetectDeath() {
         if (_currentHealth <= 0) {
             Destroy(gameObject);
         }
     }
+
 }
